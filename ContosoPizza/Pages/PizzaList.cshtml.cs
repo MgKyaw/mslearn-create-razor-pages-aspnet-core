5ed8ebee10ca57f2acef75f89d895f88e17a980e
MgKyaw <mgkyaw20112@gmail.com>
2024-07-28 21:49:46 +0700

    feat: Add OnPost() to handle Post method

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -8,7 +8,7 @@ namespace ContosoPizza.Pages
     public class PizzaListModel : PageModel
     {
         private readonly PizzaService _service;
-        public IList<Pizza> PizzaList { get;set; } = default!;
+        public IList<Pizza> PizzaList { get; set; } = default!;
         [BindProperty]
         public Pizza NewPizza { get; set; } = default!;
 
@@ -21,5 +21,18 @@ namespace ContosoPizza.Pages
         {
             PizzaList = _service.GetPizzas();
         }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
+        }
+
     }
 }
